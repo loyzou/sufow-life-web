e3d2040c2ea0b69d929846fd5f2996f8c29e26dc
sufow <git@github.com:zoushuquan>
2018-05-22 17:36:10 +0800

    Signed-off-by: sufow <git@github.com:zoushuquan>

--- src/main/java/com/sufow/action/MainAction.java
+++ src/main/java/com/sufow/action/MainAction.java
@@ -1,20 +1,41 @@
 package com.sufow.action;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.sufow.common.SufowResult;
+
 @Controller
 public class MainAction {
+	/***
+	 * 测试设置signId
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	@RequestMapping("/main/queryTest")
 	@ResponseBody
-	public Sufow queryTest(){
-		Sufow sufow = new Sufow();
-		Map<String,Object> map = new HashMap<String,Object>();
-		map.put("AAA", "CCCC");
-		return sufow;
+	public SufowResult queryTest(HttpServletRequest request,HttpServletResponse response){
+		SufowResult result = new SufowResult();
+		response.addHeader("signId", request.getSession().getId());
+		return result;
 	}
+	
+	/**
+	 * 测试返回其它项目中的对象
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping("/main/querySufowResult")
+	@ResponseBody
+	public SufowResult querySufowResult(HttpServletRequest request){
+		SufowResult result = new SufowResult();
+		result.setMessage("sufow result is success");
+		return result;
+	}
+
 }
